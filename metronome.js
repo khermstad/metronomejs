bd98c587eb5fe046931ab53d6231159960ed41e9
Kris Hermstad <krishermstad@gmail.com>
2016-05-07 23:50:13 -0400

    removed buttons for vol/bpm, changed to input(range) sliders

--- metronome.js
+++ metronome.js
@@ -2,7 +2,6 @@
 // krishermstad@gmail.com
 
 var tempVolume; // holds previous volume while on mute
-var mute = 0;   // flag to check for muted
 var currentBeat = 0; // flag to keep track of currentBeat default:0 - 4/4 -- 1/2/3/4
 
 // first set basic values, timesig, and bpm
@@ -12,7 +11,7 @@ Tone.Transport.bpm.value = 120; // default bpm
 // click sound
 // click built from Tone.NoiseSynth
 var click = new Tone.NoiseSynth({
-    "volume" : -10,
+    "volume" : -20,
     "octaves": 14,
     "envelope" : {
         "attack" : 0.001,
@@ -28,110 +27,43 @@ var click = new Tone.NoiseSynth({
 
 //loop for click
 var loop = new Tone.Loop(function(time){
-    click.triggerAttackRelease();
-    console.log(currentBeat);
-    updateBeat(); // updates currentBeat for Visual Metronome (and maybe other uses later?)
+    click.triggerAttackRelease(); // triggers click once each loop
+    console.log(currentBeat); // prints currentBeat to console
 }, "4n").start(0);
 
-//muteVolume():
-// mute: 0 = mute off. mute: 1 = mute on.
-// tempVolume holds volume before muting
-// so that it can retain original volume on unmute
-function muteVolume(){
-    if (mute == 0){
-        mute = 1;
-        tempVolume = click.volume.value;
-        click.volume.value = -100;
-    }
-    else {
-        mute = 0;
-        click.volume.value = tempVolume;
-    }
+
+// update BPM as slider is adjsuted
+function updateBPMfromSlider(){
+    // set bpm value to whatever bpmslider is currently at
+    document.getElementById('bpm').value = document.getElementById('bpmslider').value;
+    // set Tone.Transport.bpm after bpm is updated
+    Tone.Transport.bpm.value = document.getElementById('bpm').value;
 }
 
-//changeVolume(val):
-// adjusts volume of click
-function changeVolume(val){
-    var currentVolume = click.volume.value;
-    var newVolume = currentVolume + val;
-    if (newVolume < -60){
-        newVolume = -60;
-    }
-    if (newVolume > 20){
-        newVolume = 20;
-    }
-    click.volume.value = newVolume;
-    return newVolume;
-}
+// updateVolume()
+// updates click volume as slider is adjusted
+function updateVolume(){
 
-// changeBpm(val):
-// adjusts Tone.Transport.bpm.value
-function changeBpm(val) {
-    var bpm = document.getElementById('bpm').value;
-    var new_bpm = parseInt(bpm,10) + val;
-    if (new_bpm < 0) {
-        new_bpm = 0;
-    }
-    Tone.Transport.bpm.value = new_bpm; // update metronomes bpm
-    document.getElementById('bpm').value = new_bpm;
-    return new_bpm;
-}
-
+}	
 
 // play()
 // global play button for metronome
 function play(){
-    currentBeat = 1;
     Tone.Transport.start();
 }
 
 // stop()
 // global stop button for metronome
 function stop(){
-    currentBeat = 0;
-    updateBeat();
     Tone.Transport.stop();
 }
 
-// updateBeat():
-// changes img of current visual metronome
-
-function updateBeat(){
-    if (currentBeat == 0){
-        document.getElementById('circle1').src = "./img/circle50x50e.png";
-        document.getElementById('circle2').src = "./img/circle50x50e.png";
-        document.getElementById('circle3').src = "./img/circle50x50e.png";
-        document.getElementById('circle4').src = "./img/circle50x50e.png";
-    }
-    if (currentBeat == 1){
-        document.getElementById('circle1').src = "./img/circle50x50f.png";
-        document.getElementById('circle4').src = "./img/circle50x50e.png";
-        currentBeat = 2;
-    }
-    else if (currentBeat == 2){
-        document.getElementById('circle2').src = "./img/circle50x50f.png";
-        document.getElementById('circle1').src = "./img/circle50x50e.png";
-        currentBeat = 3;
-    }
-    else if (currentBeat == 3){
-        document.getElementById('circle3').src = "./img/circle50x50f.png";
-        document.getElementById('circle2').src = "./img/circle50x50e.png";
-        currentBeat = 4;
-    }
-    else if (currentBeat == 4){
-        document.getElementById('circle4').src = "./img/circle50x50f.png";
-        document.getElementById('circle3').src = "./img/circle50x50e.png";
-        currentBeat = 1;
-    }
-}
 
 // reset():
 // resets all values to default, also stops if playing.
 function reset(){
     Tone.Transport.stop();
-    click.volume.value = -10; // changes volume back to default value
-    document.getElementById('bpm').value = 120; // sets new bpm to bpm textfield
-    Tone.Transport.bpm.value = 120; // sets global bpm back to 120
-    currentBeat = 0;
-    updateBeat();
+    click.volume.value = -20; // changes volume back to default value
+    document.getElementById('bpmslider').value = 120; // sets new bpm to bpm textfield
+    updateBPMfromSlider();
 }
